04416fce674b845e844a73db9b4c212cc19fa3f5
Olumide Adewole <olumide.adewole@sabi.am>
2024-11-08 08:58:37 +0100

    Test upload

---
+++ test.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    $name = "Dani Phantom";
+
+    echo $name;
+
+    //scaler variables
+    $string = "Dan";
+    $int = 22345;
+    $float = 23.454;
+    $bool = false;
+
+    $names = "Daniel";
+
+    ?>
+
+</body>
+
+</html>
